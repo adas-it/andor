cfac3a4e74ba32367a7d4f9f6d3e6f88ac460f7f
Lucas de Oliveira <lucashigor@outlook.com>
2024-01-29 08:48:43 +0000

    Docker compose

--- src/Family.Budget.Domain/SeedWork/ShearchableRepository/IShearchableRepository.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Family.Budget.Domain.SeedWork.ShearchableRepository;
-
-public interface IShearchableRepository<T> where T : Entity
-{
-    Task<SearchOutput<T>> Search(SearchInput input, CancellationToken cancellationToken);
-}
